617a9bf274c4e05ef0e6d5fc25980b9bbca7a799
Scott Atkinson <scott.atkin79@gmail.com>
2023-05-27 08:54:57 +0000

    set up html,css and js files and basic game set up

---
+++ script.js
@@ -0,0 +1,34 @@
+addEventListener('load', function () {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 2500;
+    canvas.height = 1500;
+
+    class InputHandler {
+
+    }
+
+    class Projectile {
+
+    }
+
+    class Particle {
+
+    }
+
+    class Player {
+
+    }
+
+    class Enemy {
+
+    }
+
+    class Layer {
+
+    }
+
+    class background {
+
+    }
+});
