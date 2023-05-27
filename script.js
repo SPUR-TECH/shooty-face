d79b6c8b07efd07cdd0ba9d91ddcc9b68f94f126
Scott Atkinson <scott.atkin79@gmail.com>
2023-05-27 10:13:26 +0000

    create player and animation loop

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 addEventListener('load', function () {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = 2500;
-    canvas.height = 1500;
+    canvas.width = 500;
+    canvas.height = 500;
 
     class InputHandler {
-
+        constructor(game) {
+            this.game = game;
+        }
     }
 
     class Projectile {
@@ -17,6 +19,22 @@ addEventListener('load', function () {
     }
 
     class Player {
+        constructor(Game) {
+            this.Game = Game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedY = 0;
+        }
+
+        update() {
+            this.y += this.speedY;
+        }
+
+        draw(context) {
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
 
     }
 
@@ -31,4 +49,34 @@ addEventListener('load', function () {
     class background {
 
     }
+
+    class UI {
+
+    }
+
+    class Game {
+        constructor(width, height) {
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+        }
+
+        update() {
+            this.player.update();
+        }
+
+        draw(context) {
+            this.player.draw(context)
+        }
+    }
+
+    const game = new Game(canvas.width, canvas.height);
+    // Animation loop
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.update();
+        game.draw(ctx);
+        requestAnimationFrame(animate);
+    }
+    animate();
 });
