7f0191d17a5a5c704e36328a24518a1961d59abb
Scott Atkinson <scott.atkin79@gmail.com>
2023-05-27 18:19:35 +0000

    draw projectiles and player

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 addEventListener('load', function () {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = 1500;
+    canvas.width = 900;
     canvas.height = 500;
 
     let startingY, movingY;
@@ -50,21 +50,30 @@ addEventListener('load', function () {
             this.game = game;
             this.x = x;
             this.y = y;
-            this.width = 10;
-            this.height = 3;
-            this.speed = 3;
+            this.width = 36.25;
+            this.height = 20;
+            this.speed = Math.random() * 0.2 + 2.8;
             this.markedForDeletion = false;
+            this.image = document.getElementById('fireball');
+            this.frameX = 0;
+            this.maxFrame = 3;
+            this.fps = 10;
+            this.timer = 0;
+            this.interval = 1000 / this.fps;
         }
-
-        update() {
+        update(deltaTime) {
             this.x += this.speed;
+            if (this.timer > this.interval) {
+                if (this.frameX < this.maxFrame) this.frameX++;
+                else this.frameX = 0;
+                this.timer = 0;
+            } else {
+                this.timer += deltaTime;
+            }
             if (this.x > this.game.width * 0.8) this.markedForDeletion = true;
         }
-
         draw(context) {
-            context.fillStyle = 'yellow';
-            context.fillRect(this.x, this.y, this, this.width, this.height)
-
+            context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height);
         }
     }
 
@@ -79,9 +88,13 @@ addEventListener('load', function () {
             this.height = 190;
             this.x = 20;
             this.y = 100;
+            this.frameX = 0;
+            this.frameY = 0;
+            this.maxFrame = 37;
             this.speedY = 0;
             this.maxSpeed = 4;
             this.projectiles = [];
+            this.image = document.getElementById('player');
         }
 
         update() {
@@ -99,11 +112,17 @@ addEventListener('load', function () {
                 projectile.update();
             });
             this.projectiles = this.projectiles.filter(projectile => !projectile.markedForDeletion);
+
+            // sprite animation
+            if (this.frameX < this.maxFrame) {
+                this.frameX++;
+            } else {
+                this.frameX = 0;
+            }
         }
 
         draw(context) {
-            context.fillStyle = 'black'
-            context.fillRect(this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
             this.projectiles.forEach(projectile => {
                 projectile.draw(context);
             });
